c5292563a81b2ed12eb1019b8ba6d13ab9d5f1a2
Justin <justin.salvador@student.csulb.edu>
2021-03-04 18:22:49 -0800

    Created Powertrain .ino File

---
+++ Software/Powertrain/Test/Test/Test.ino
@@ -0,0 +1,12 @@
+const int hello = 10;
+const int conflict_again_again = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
